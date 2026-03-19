96c6b5ad10bb48eabbb724f168432e3dc2283101
Nave Axel <axel.nave0706@gmail.com>
2026-03-19 19:11:41 +0100

    authentification réalisé (#10)

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomePage from './pages/HomePage.vue'
+import SignInPage from './pages/SignInPage.vue'
+import SignUpPage from './pages/SignUpPage.vue'
 
 export const ROUTES = {
   HOME: '/',
+  SIGN_IN: '/sign-in',
+  SIGN_UP: '/sign-up',
 } as const
 
 const routes = [
   { path: ROUTES.HOME, component: HomePage, meta: { requiresAuth: true } },
+  {
+    path: ROUTES.SIGN_IN,
+    component: SignInPage,
+    meta: { requiresGuest: true },
+  },
+  {
+    path: ROUTES.SIGN_UP,
+    component: SignUpPage,
+    meta: { requiresGuest: true },
+  },
 ]
 
 const router = createRouter({
